2569c3e38c8d6ca9fd008a597240750041b22762
Marcosbc05 <marcosbarrosocarmonamp@gmail.com>
2024-12-12 13:22:14 +0100

    permisos funcion

--- PL3/users.sql
+++ PL3/users.sql
@@ -17,7 +17,7 @@ GRANT ALL PRIVILEGES ON TABLE auditoria TO admin;
 
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA base_discos FROM gestor;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA base_discos TO gestor;
-GRANT SELECT, INSERT, UPDATE ON TABLE auditoria TO gestor;
+GRANT SELECT, INSERT ON TABLE auditoria TO gestor;
 
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA base_discos FROM cliente;
 GRANT SELECT, INSERT ON base_discos.Tiene, base_discos.Desea, base_discos.Grupo, base_discos.Disco, base_discos.Cancion, base_discos.Edicion TO cliente;
@@ -26,5 +26,8 @@ GRANT SELECT, INSERT ON TABLE auditoria TO cliente;
 
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA base_discos FROM invitado;
 GRANT SELECT ON base_discos.Grupo, base_discos.Disco, base_discos.Cancion TO invitado;
+
+GRANT EXECUTE ON FUNCTION auditoria_funcion() TO admin, gestor, cliente;
+GRANT EXECUTE ON FUNCTION comprobar_lista_deseados() TO admin, gestor, cliente;
 
 COMMIT;
